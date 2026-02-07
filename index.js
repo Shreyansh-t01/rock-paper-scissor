1b8a0a83071b9c388fe62646b99841194714ef5b
shreyansh <shreyanshthakur2007@gmail.com>
2026-02-07 15:59:35 +0530

    js logics completed behind rock paper scissor

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 let arr=['Paper.png','Rock.png','Scissor.png'];
 
-let text="Timer";
+let ind=0;
+let  computer=['paper','rock','scissor'];
+let computerinput='computer';
 
 
 function Computer(){
-     
-    document.getElementById("img").src=arr[Math.floor(Math.random()*3)];
+     ind=Math.floor(Math.random()*3);
+     computerinput=computer[ind];
+     console.log(computerinput)
+    document.getElementById("img").src=arr[ind];
+
+
+
+    
+    if(computerinput===userinput){
+    document.getElementById("result").innerText='Tie!'
+    }
+   else if((computerinput==='rock' && userinput==='scissor')||(computerinput==='paper' && userinput==='rock') ||(computerinput==='scissor' && userinput==='paper')){
+    document.getElementById("result").innerText='Your PC wins 😉'
+   }
+     else if((computerinput==='rock' && userinput==='paper')|| (computerinput==='paper' && userinput==='scissor')||(computerinput==='scissor' && userinput==='rock')){
+     document.getElementById("result").innerText='You Won, looks you are smarter than your PC 🙃';
+    }
      
      }
+
+
+
+
 let end=false;
 let count=3;
 let btn = document.querySelector("button");
-console.log(btn)
+
+
+
 btn.addEventListener('click',function(){
    
     if(end==false){
@@ -41,14 +64,42 @@ btn.addEventListener('click',function(){
   
 })
 
-// game logic
+
+
+
+
+
+let userinput='input';
+
+
+document.addEventListener('keydown',function(event){
+    if(event.key==='1'){
+        userinput='rock';
+    }
+   else  if(event.key==='2'){
+          userinput='paper';
+    }
+    else if(event.key==='3'){
+        userinput='scissor';
+    }
+    document.getElementById("input").innerText=userinput;
+})
+
+
+
+
+
+
+
+
+
 
 
 
 
 
   
-    l
+    
   
 
  
